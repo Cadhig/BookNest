0b43bcc07de1bc9fa693e1131f0dba4623c033ee
Cadence K <cadencehiggins@gmail.com>
2024-08-08 09:48:14 -0500

    added posts to server & index

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const userApi = require('./User.js')
+const postsApi = require('./Posts.js')
 
 router.use('/api/user', userApi)
+router.use('/api/posts', postsApi)
 
 module.exports = router
