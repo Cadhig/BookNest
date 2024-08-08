8a4782c3736874b7b9ef189199c41b38ede4f593
Cadence K <cadencehiggins@gmail.com>
2024-08-07 22:55:45 -0500

    began posts api routes

--- server/models/Posts.js
+++ server/models/Posts.js
@@ -25,4 +25,4 @@ const postSchema = new mongoose.Schema({
     comments: [commentSchema]
 })
 
-module.exports = mongoose.model("Posts", postSchema)
+module.exports = mongoose.model("Post", postSchema)
