95ae87541361b6923cc9fbc227d794027772d76e
Cadence K <cadencehiggins@gmail.com>
2024-08-08 09:47:04 -0500

    created Posts routes

--- server/api/Posts.js
+++ server/api/Posts.js
@@ -19,3 +19,23 @@ router.get('/', (req, res) => {
 
 })
 
+router.post('/', (req, res) => {
+    const { postText } = req.body
+    if (!req.session.user_id) {
+        return res.status(401).json({ error: "Unauthorized" })
+    }
+    Posts.where("user_id").equals(req.session.user_id).create({
+        postText: postText,
+        user_id: req.session.user_id
+    })
+        .then((result) => {
+            return res.status(200).json(result)
+        })
+        .catch((err) => {
+            console.error(err)
+            return res.status(400).json({
+                message: 'Could not create post!'
+            })
+        })
+})
+
