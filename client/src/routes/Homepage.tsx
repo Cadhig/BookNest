fc7464b9abb5b8b6e014292f20ab98818329c3f8
Cadence K <cadencehiggins@gmail.com>
2024-08-10 13:57:21 -0500

    created types for NYT api response, mapping through data in Homepage.tsx

--- client/src/routes/Homepage.tsx
+++ client/src/routes/Homepage.tsx
@@ -1,16 +1,20 @@
 import { useEffect, useState } from "react"
+import { ApiResponse } from "../types"
 
 export default function Homepage() {
-    const [apiData, setApiData] = useState()
+    const [apiData, setApiData] = useState<ApiResponse>()
     useEffect(() => {
         fetch(`https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${import.meta.env.VITE_NYT_API_KEY}`)
             .then(res => res.json())
             .then(data => setApiData(data))
+            .catch((err) => console.error(err))
     }, [])
     console.log(apiData)
     return (
         <div>
-            homepage
+            {apiData && apiData.results.books.map((content, index) => {
+                return <div key={index}>{content.title}</div>
+            })}
         </div>
     )
 }
