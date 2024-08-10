97c7a105c8f939578562ab55b8ecff89fc7983d4
Cadence K <cadencehiggins@gmail.com>
2024-08-10 13:13:13 -0500

    fixed cors issue with nyt api

--- client/src/routes/Homepage.tsx
+++ client/src/routes/Homepage.tsx
@@ -1,9 +1,13 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export default function Homepage() {
+    const [apiData, setApiData] = useState()
     useEffect(() => {
-        fetch(`https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=${import.meta.env.VITE_API_KEY}`)
+        fetch(`https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${import.meta.env.VITE_NYT_API_KEY}`)
+            .then(res => res.json())
+            .then(data => setApiData(data))
     }, [])
+    console.log(apiData)
     return (
         <div>
             homepage
