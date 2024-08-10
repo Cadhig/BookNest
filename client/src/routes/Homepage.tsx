9e6e1256491bd8c78d8ffa5ebc39648e1d5aeb56
Cadence K <cadencehiggins@gmail.com>
2024-08-10 10:55:23 -0500

    implemented google api with key in env file

--- client/src/routes/Homepage.tsx
+++ client/src/routes/Homepage.tsx
@@ -1,4 +1,9 @@
+import { useEffect } from "react"
+
 export default function Homepage() {
+    useEffect(() => {
+        fetch(`https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=${import.meta.env.VITE_API_KEY}`)
+    }, [])
     return (
         <div>
             homepage
