8f356a9fe7c1e21b2dd0b6233ff3ae64d7746c9a
Cadence K <cadencehiggins@gmail.com>
2024-08-18 16:11:36 -0500

    adjusted radius on search bar

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -6,8 +6,8 @@ export default function SearchBar() {
     return (
         <div className="w-full md:w-1/4">
             <div className="flex gap-2 w-full justify-center">
-                <input type="text" placeholder='e.g "Colleen Hoover"' className="border border-book-sage w-3/4 md:w-1/2 flex-shrink-0 " onChange={(e) => setBook(e.target.value)} />
-                <Link to={'/results'} state={{ from: { book } }}><button className="bg-book-green text-book-light p-2">Search</button></Link>
+                <input type="text" placeholder='e.g "Colleen Hoover"' className="border border-book-sage w-3/4 md:w-1/2 flex-shrink-0 rounded-full p-2" onChange={(e) => setBook(e.target.value)} />
+                <Link to={'/results'} state={{ from: { book } }}><button className="bg-book-green text-book-light py-2 px-4 rounded-full">Search</button></Link>
             </div>
         </div>
     )
