9ad537de5dbf944c90595bbda380ee5b27e00c3d
Cadence K <cadencehiggins@gmail.com>
2024-09-02 19:49:06 -0500

    added feed type buttons to homepage

--- client/src/routes/Homepage.tsx
+++ client/src/routes/Homepage.tsx
@@ -7,8 +7,12 @@ import Sidebar from "../components/Sidebar"
 import Posts from "../components/Posts"
 
 export default function Homepage() {
-    const [mobileMenu, setMobileMenu] = useState('hidden')
-    const [refreshFeed, setRefreshFeed] = useState(false)
+    const [mobileMenu, setMobileMenu] = useState<string>('hidden')
+    const [refreshFeed, setRefreshFeed] = useState<boolean>(false)
+    const [globalBar, setGlobalBar] = useState<string>("bg-book-green rounded-full w-1/4 h-[2px]")
+    const [globalText, setGlobalText] = useState<string>("w-1/2 flex flex-col items-center text-xl font-bold")
+    const [followingBar, setFollowingbar] = useState<string>('hidden')
+    const [followingText, setFollowingText] = useState<string>("w-1/2 flex flex-col items-center text-xl text-book-dark/60")
 
     function toggleMobileMenu(val: boolean) {
         if (val) {
@@ -18,14 +22,46 @@ export default function Homepage() {
         }
     }
 
+    function switchFeedType(feedType: string) {
+        if (feedType === 'global') {
+            showGlobal()
+        } else {
+            showFollowing()
+        }
+    }
+
+    function showGlobal() {
+        setGlobalText('w-1/2 flex flex-col items-center text-xl font-bold')
+        setGlobalBar('bg-book-green rounded-full w-1/4 h-[2px]')
+        setFollowingbar('hidden')
+        setFollowingText('w-1/2 flex flex-col items-center text-xl text-book-dark/60')
+    }
+
+    function showFollowing() {
+        setGlobalText('w-1/2 flex flex-col items-center text-xl text-book-dark/60')
+        setGlobalBar('hidden')
+        setFollowingbar('bg-book-green rounded-full w-1/4 h-[2px]')
+        setFollowingText('w-1/2 flex flex-col items-center text-xl font-bold')
+    }
+
 
     return (
-        <div className="h-svh">
+        <div className="h-svh default-font text-book-dark">
             <MobileMenu mobileMenu={mobileMenu} />
             <MobileHeader toggleMobileMenu={toggleMobileMenu} />
             <div onClick={() => toggleMobileMenu(false)} className="flex flex-col md:flex-row-reverse">
                 <SearchBar />
                 <div className="flex flex-col md:w-1/2 w-full max-h-full">
+                    <div className="flex my-4">
+                        <div className={globalText} onClick={() => switchFeedType('global')}>
+                            <button>Global</button>
+                            <div className={globalBar}></div>
+                        </div>
+                        <div className={followingText} onClick={() => switchFeedType('following')}>
+                            <button>Following</button>
+                            <div className={followingBar}></div>
+                        </div>
+                    </div>
                     <CreatePost setRefreshFeed={setRefreshFeed} refreshFeed={refreshFeed} />
                     <Posts refreshFeed={refreshFeed} />
                 </div>
