afa71f7a09ad78a6a6c1ad582ad4d219d4dee2ab
Cadence K <cadencehiggins@gmail.com>
2024-09-18 11:04:58 -0500

    replaced placeholder for search bar

--- client/src/components/RightSidebar.tsx
+++ client/src/components/RightSidebar.tsx
@@ -6,7 +6,7 @@ export default function RightSidebar() {
     return (
         <div className="w-full lg:w-1/4 mt-4">
             <div className="flex gap-2 w-full justify-center">
-                <input type="text" placeholder='e.g "Colleen Hoover"' className="border border-book-sage w-3/4 lg:w-1/2 flex-shrink-0 rounded-full p-2" onChange={(e) => setBook(e.target.value)} />
+                <input type="text" placeholder='Search by Author or Title' className="border border-book-sage w-3/4 lg:w-1/2 flex-shrink-0 rounded-full p-2" onChange={(e) => setBook(e.target.value)} />
                 <Link to={'/results'} state={{ search: { book } }}><button className="bg-book-green text-book-light py-2 px-4 rounded-full hover:bg-book-green-hover">Search</button></Link>
             </div>
         </div>
